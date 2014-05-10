61ccb7dbdfc9301dead9893b5c3d456f25c241c8
SvSchmidt <wqFrAABhMHRN4S1pk8lJ>
2014-05-10 19:14:31 +0200

    v1.0.0

---
+++ guid.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Calculates a globally unique identifier (GUID)
+ * @return String GUID (e.g. {D3232798-88C4-E335-9AED-67B308BA5CD8})
+ */
+function guid() {
+	if(function_exists("com_create_guid")) return com_create_guid();
+
+	$chars = strtoupper(md5(uniqid(rand(),true)));
+	return "{".
+			substr($chars,0,8)."-".
+			substr($chars,8,4)."-".
+			substr($chars,12,4)."-".
+			substr($chars,16,4)."-".
+			substr($chars,20,12).
+			"}";
+
+}
+ 
+print guid();
+
+?>
